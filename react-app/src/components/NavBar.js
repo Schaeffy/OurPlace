b30514ee63c8ba36e001675ca5b7d1b05afe1829
Schaeffer <schaefferahn@gmail.com>
2022-11-23 15:12:53 -0800

    Working on home page

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,36 +2,62 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import ourplaceLogo from './images/ourplace3.png';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+
+    <div className='nav-container'>
+
+      <div className='top-nav-bar'>
+        <div className='nav-left-container'>
+          <div className='nav-left-image'>
+            <img id='logo' src={ourplaceLogo} alt='logo' />
+          </div>
+        </div>
+
+        <div className='nav-right-container'>
+          <div className='nav-right-links'>
+            <div className='nav-link'>
+              <NavLink to='/help' exact={true} activeClassName='active'>
+                Help
+              </NavLink>
+
+              <NavLink id='login' to='/login' exact={true} activeClassName='active'>
+                LogIn
+              </NavLink>
+
+              <NavLink id='signup' to='/sign-up' exact={true} activeClassName='active'>
+                SignUp
+              </NavLink>
+
+
+              <LogoutButton />
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className='bottom-nav-bar'>
+
+              <NavLink to='/' exact={true} activeClassName='active'>
+                Home
+              </NavLink>
+
+              <NavLink to='/users' exact={true} activeClassName='active'>
+                Browse
+              </NavLink>
+
+              <NavLink to='/blogs/' exact={true} activeClassName='active'>
+                Blogs
+              </NavLink>
+
+
+      </div>
+
+    </div>
   );
 }
 
