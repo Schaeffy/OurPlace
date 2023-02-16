5a29c84e5c0f2c03da69a15c37e16ca0ee8ec870
Schaeffer <schaefferahn@gmail.com>
2023-02-16 14:51:11 -0800

    foreign key

--- app/models/profile_pic.py
+++ app/models/profile_pic.py
@@ -8,7 +8,8 @@ class ProfilePic(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod('users.id')), nullable=False)
     url = db.Column(db.String(255), nullable=False)
 
     user = db.relationship("User", back_populates="profile_pic", cascade="all")
